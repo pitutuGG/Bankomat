bc77d88dda013baa9c3c416681c837d5297b2534
a.sysio <112017528+pitutuGG@users.noreply.github.com>
2022-12-01 22:26:48 +0100

    Add files via upload

---
+++ bankomat/bankomat.js
@@ -0,0 +1,10 @@
+// alert("Wybierz kartę")
+document.getElementById("wyswietlacz").innerHTML = "Wybierz kartę"
+
+document.getElementById("button").onclick = function(){
+    alert("Niestety nie obsługujemy innych kart");
+}
+
+document.getElementById("cards").onclick = function(){
+    
+}
